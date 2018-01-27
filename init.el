dac676c2e52d86b580c4c041527f330964ccec51
kichi ryuzaki <kichi.ryuzaki@gmail.com>
2018-01-28 01:31:09 +0900

    init.elに余計なものが入っていたので戻した

--- init.el
+++ init.el
@@ -43,21 +43,3 @@
 
 ;;  my下のカスタム設定を有効にする
 (load "_my_init.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-	("eb6196d0b5baa26be14ff9f506f350e2c729e35e658bf308abed11916553fdad" "188081e09548900e09a194aa4e0551ffa16006ca6bdee1b03e6b6e810bc8dfcb" "b8f2c10cdba494b863d6d1830622101a8675d37abde0c75ed3ee07de9ad2e434" "447e7f85b30943425823306ee1bf4b184c5aeb78bf179186b827d8f580dc9f14" default)))
- '(package-selected-packages
-   (quote
-	(docker-compose-mode cmake-mode csharp-mode auto-complete sr-speedbar ddskk base16-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
